1c8fd7a655a8c5c10826f62adc46833a96db21ab
raimondas-27 <raimondasvilnius@yahoo.com>
2021-08-31 11:54:04 +0300

    some changes

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-// jei mes importuojam kazka kas bus naudojama tik getServerSideProps arba getStaticProps ...
-// jie nebuna prideti prie galutinio react componento
 import MeetupList from '../components/meetups/MeetupList';
 import { getCollection } from '../utils/mongo-data';
 import Head from 'next/head';
 import { SITE_NAME } from '../utils/config';
 
 const HomePage = (props) => {
-  // title = All Meetups - React Meetups
+
   return (
     <>
       <Head>
@@ -19,31 +17,15 @@ const HomePage = (props) => {
   );
 };
 
-// tam kad puslapis butu sugeneruotas duomenims pasikeitus yra naudojami 2 budai
-
-// SSR server side rendering getServerSideProps - duomenys sugeneruojami uzklausom metus, tinka labiau kai duomenys kinta kas sekunde ar greiciau
-// export function getServerSideProps(context) {
-//   // sitas kodas niekada neatsidurs pas clienta, cia galima sakyti yra back end erdve
-//   const req = context.req;
-//   const res = context.res;
-//   // console.log(req);
-//   return {
-//     props: {
-//       meetups: DUMMY_MEETUPS,
-//     },
-//   };
-// }
-// SSG static site Generatingas getStaticProps() = duomeny sugeneruojami aplikacijos sukurimo metu ir atnaujinami jei reikia tam tikru intervalu
 export async function getStaticProps() {
-  // sitas kodas niekada neatsidurs pas clienta, cia galima sakyti yra back end erdve
-  // fetch, validacija ir pan
+
   const [meetupCollecion, client] = await getCollection();
   const allMeets = await meetupCollecion.find({}).toArray();
-  client.close();
-  console.log('All meeets transformed ============');
+  await client.close();
+
   // console.log(allMeets);
   const meetsInReqFormat = allMeets.map((dbObj) => {
-    // _id yra ObjectId ir gausim klaida jei bandysim nuskaityti ji kaip string jsx
+
     return {
       id: dbObj._id.toString(),
       title: dbObj.title,
@@ -57,7 +39,7 @@ export async function getStaticProps() {
     props: {
       meetups: meetsInReqFormat,
     },
-    revalidate: 2, // kas 10 sek duomenys bus atnaujinami
+    revalidate: 2,
   };
 }
 
